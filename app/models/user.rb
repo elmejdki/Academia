e959cfd14bf3df95ebb9c6d7f0a14c6582ee73a5
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-07-03 05:02:12 +0100

    Add who to follow section

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,20 @@ class User < ApplicationRecord
   has_many :followers, class_name: 'Following', foreign_key: 'followed_id'
 
   has_many :followings, class_name: 'Following', foreign_key: 'follower_id'
+
+  def follower?(user)
+    self.followers.where(follower_id: user.id).count > 0
+  end
+
+  def following?(user)
+    self.followings.where(followed_id: user.id).count > 0
+  end
+
+  def out_users
+    User.all.filter{ |user| !self.following?(user) && self != user }
+  end
+
+  def followed_by
+    self.followers.limit(1)
+  end
 end
