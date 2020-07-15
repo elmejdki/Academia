ee5fa7ca73bca2c2ab699054a5e0e3e95a82eecb
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-07-15 18:27:20 +0100

    Add view helpers for rooms view

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,47 @@
 module RoomsHelper
+  def there_is_no_room?
+    if @rooms.empty?
+      render 'no_room'
+    end
+  end
+
+  def get_rooms(room)
+    user = room.side_user(current_user)
+
+    if user
+      render partial: 'room_link', locals: { room: room, user: user }
+    end
+  end
+
+  def get_room_user_image(user, className)
+    if user.avatar.attached?
+      render partial: 'room_user_image', locals: { user: user, className: className }
+    else
+      render partial: 'room_user_fake_image', locals: { className: className }
+    end
+  end
+
+  def get_message_notice(room)
+    if room.messages.last.user_id != current_user.id && room.messages.last.unread
+      render 'message_notice'
+    end
+  end
+
+  def get_room
+    if @room.present?
+      render 'message_box'
+    else
+      render 'select_room'
+    end
+  end
+
+  def get_message_text(msg, index)
+    unless msg.body == "nil"
+      if index == @message_number
+        render 'message_ruler'
+      end
+     
+      render partial: 'message', locals: { msg: msg }
+    end
+  end
 end
